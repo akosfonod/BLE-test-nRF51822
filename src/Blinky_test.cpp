880aea882ebc2065e6d59314bc7bd691ac062eb8
Akos Fonod - Acer <akosfonod@gmail.com>
2020-09-06 00:20:41 +0200

    Modifying PIO project file and adding placeholder source file.

---
+++ src/Blinky_test.cpp
@@ -0,0 +1,7 @@
+//Waveshare_BLE400 pio doc: https://docs.platformio.org/en/latest/boards/nordicnrf51/waveshare_ble400.html
+//nRF51822 platform doc:    https://docs.platformio.org/en/latest/platforms/nordicnrf51.html#platform-nordicnrf51
+//nRF51922 lib doc:         https://platformio.org/lib/show/2587/nRF51822
+//BLEPheriperal lib:        https://platformio.org/lib/show/259/BLEPeripheral
+//Arduino+nRF51822+pIO:     https://docs.platformio.org/en/latest/tutorials/nordicnrf52/arduino_debugging_unit_testing.html
+//stlink debugging:         https://docs.platformio.org/en/latest/plus/debug-tools/stlink.html
+//BlackMagicProbe debugging:https://docs.platformio.org/en/latest/plus/debug-tools/blackmagic.html
