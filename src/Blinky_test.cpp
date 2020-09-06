971dc9ee6639b781fff73e207e06b4b0da3f993a
Akos Fonod - Acer <akosfonod@gmail.com>
2020-09-06 15:07:34 +0200

    Adding blink example code.

--- src/Blinky_test.cpp
+++ src/Blinky_test.cpp
@@ -5,3 +5,33 @@
 //Arduino+nRF51822+pIO:     https://docs.platformio.org/en/latest/tutorials/nordicnrf52/arduino_debugging_unit_testing.html
 //stlink debugging:         https://docs.platformio.org/en/latest/plus/debug-tools/stlink.html
 //BlackMagicProbe debugging:https://docs.platformio.org/en/latest/plus/debug-tools/blackmagic.html
+//Unit testing on HW:       https://docs.platformio.org/en/latest/plus/unit-testing.html
+
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include <Arduino.h>
+
+#define LED 20
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED, LOW);
+   // wait for a second
+  delay(1000);
+}
+
