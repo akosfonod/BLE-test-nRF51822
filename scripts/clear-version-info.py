8df2a72bf7e30bdf0507a9def41c0c06e1b91a3b
Akos Fonod - Acer <akosfonod@gmail.com>
2020-10-08 23:21:14 +0200

    commit id script part #2.

--- scripts/clear-version-info.py
+++ scripts/clear-version-info.py
@@ -3,9 +3,9 @@ Import ("env")
 def clear_commit_id(source, target, env):
     print("----Post-Build script----")
     print("Rewoking commit ID details.")
-    version_file = open('include/git-version.h','w')
-    version_file.write('#ifndef GIT_VERSION_H\n#define GIT_VERSION_H\n    #define GIT_COMMIT_ID "0000000"\n#endif')
-    version_file.close()
+    # version_file = open('include/git-version.h','w')
+    # version_file.write('#ifndef GIT_VERSION_H\n#define GIT_VERSION_H\n    #define GIT_COMMIT_ID "0000000"\n#endif')
+    # version_file.close()
     print("----Post-Build script END ----")
 
 env.AddPostAction("buildprog", clear_commit_id)
